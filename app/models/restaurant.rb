c98ea916c772d2584705b62c2cdf7cef12fd3239
youenn priser <youenn.priser@gmail.com>
2020-02-26 12:55:25 +0100

    Changed the dependence keyword in restaurant reviews relation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :delete_all
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
