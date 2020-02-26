e334b1dc0dc3c0c9192c666534f89102bd28755c
youenn priser <youenn.priser@gmail.com>
2020-02-26 14:33:48 +0100

    Added seeds to the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+print 'Cleaning DB...........'
+Restaurant.destroy_all
+puts 'OK'
+
+print 'Seeding restaurants...'
+6.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+
+  rand(1..3).times do
+      Review.create!(
+        content: Faker::Restaurant.review,
+        rating: rand(1..5),
+        restaurant: restaurant
+      )
+  end
+end
+puts 'OK'
